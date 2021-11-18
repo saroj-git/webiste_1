715cdc959becac666957c2b4c96b63f3dbd3a27e
herb108 <aaron.eagle123@gmail.com>
2021-11-18 23:38:39 +0400

    add about url

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("This is http response from the polls app")
+
+def about(request):
+    return HttpResponse("This is about page from user_2")
